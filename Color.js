1c89fc0cec45fad743f131ab3269be5134fdbd0d
Shane Warning <shane.k.warning@gmail.com>
2022-02-25 22:13:43 -0600

    Remove unneeded values

--- Color.js
+++ Color.js
@@ -1,14 +1,12 @@
 class Color {
   constructor() {
     this.ranHexCode = [];
-    this.counter = 0;
     this.locked = false;
   }
   randomHex(close) {
     var array = []
     var hexcode = []
-    for (var i = 0; hexcode.length < 6; i++) {
-      this.counter ++
+    for (var i = 0; i < 6; i++) {
       array = hexCodeArray[getRandomIndex(hexCodeArray)]
       hexcode.push(array)
     }
