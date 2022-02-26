69fd33c410158327d88c1e704fa21d875a5f6563
Shane Warning <shane.k.warning@gmail.com>
2022-02-25 21:34:17 -0600

    Add global variables

--- data.js
+++ data.js
@@ -1,14 +1,10 @@
-function uniqueId() {
-  return Math.floor((Math.random() * 1000) + 1)
-}
-var palleteId;
-paletteId = uniqueId()
-
-var newColorArray = new Color
-
 var fiveHexCodes = [];
 var hexCodeArray = ['A','B','C','D','E','F','0','1',
 '2','3','4','5','6','7','8','9']
 
+
+
+
 var currentPalette = new Palette;
 var newPalette = new Palette
+var newColorArray = new Color
