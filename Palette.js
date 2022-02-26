54e5f2c033e582a51309dfa35435e00101f66c84
Shane Warning <shane.k.warning@gmail.com>
2022-02-26 12:05:52 -0600

    Edit dot notation to match new output from Palette Class

--- Palette.js
+++ Palette.js
@@ -6,12 +6,11 @@ class Palette {
   }
 
   accessColor() {
+    this.colorPalette = []
   for (var i = 0; i <= 4; i++) {
     var testColorArray = new Color
     testColorArray.randomHex()
     this.colorPalette.push(testColorArray)
-    console.log(testColorArray)
   }
-    console.log(this.colorPalette)
 }
 }
