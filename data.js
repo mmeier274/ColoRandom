c48f1cd01a5c381a6b120e7ee469c0e0deae1d1a
Shane Warning <shane.k.warning@gmail.com>
2022-02-25 21:09:01 -0600

    Add data file

---
+++ data.js
@@ -0,0 +1,14 @@
+function uniqueId() {
+  return Math.floor((Math.random() * 1000) + 1)
+}
+var palleteId;
+paletteId = uniqueId()
+
+var newColorArray = new Color
+
+var fiveHexCodes = [];
+var hexCodeArray = ['A','B','C','D','E','F','0','1',
+'2','3','4','5','6','7','8','9']
+
+var currentPalette = new Palette;
+var newPalette = new Palette
