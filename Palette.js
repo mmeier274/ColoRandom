27bc5f2a26e3f4d4a2e907ff342c09b4b4bd3065
Shane Warning <shane.k.warning@gmail.com>
2022-02-26 11:47:14 -0600

    Move decaration of testColorArray inside for loop

--- Palette.js
+++ Palette.js
@@ -1,14 +1,17 @@
 class Palette {
   constructor() {
     this.colorPalette =[]
+    this.lockCheck = []
     this.uniqueId = Date.now()
   }
+
   accessColor() {
-    var testColorArray = new Color
-    this.colorPalette = []
   for (var i = 0; i <= 4; i++) {
+    var testColorArray = new Color
     testColorArray.randomHex()
-    this.colorPalette.push(testColorArray.ranHexCode)
+    this.colorPalette.push(testColorArray)
+    console.log(testColorArray)
   }
+    console.log(this.colorPalette)
 }
 }
