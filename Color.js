cf67039d0c9428a6784c50f2bfa2566773ef6b30
Shane Warning <shane.k.warning@gmail.com>
2022-02-28 13:28:34 -0600

    Edit variable names to help readablilty

--- Color.js
+++ Color.js
@@ -5,14 +5,15 @@ class Color {
   }
   randomHex() {
     var array = []
-    var hexCodeArray = ['A','B','C','D','E','F','0','1',
-    '2','3','4','5','6','7','8','9']
+    var hexCodeArray = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1',
+      '2', '3', '4', '5', '6', '7', '8', '9'
+    ]
     var hexcode = []
     for (var i = 0; i < 6; i++) {
       array = hexCodeArray[getRandomIndex(hexCodeArray)]
       hexcode.push(array)
     }
-    var test = `#${hexcode.join('')}`
-    return test
+    var spliceHex = `#${hexcode.join('')}`
+    return spliceHex
   }
 }
