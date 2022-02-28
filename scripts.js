abf43d4672abd6173c8ecdeb93fc6252e4a5925e
Shane Warning <shane.k.warning@gmail.com>
2022-02-27 22:16:03 -0600

    Remove querySelectors replaced with selectorAll refactor toggle event

--- scripts.js
+++ scripts.js
@@ -2,16 +2,6 @@ var allColor = document.querySelectorAll('.color')
 var allHex = document.querySelectorAll('.hex')
 var lockBtnAll = document.querySelectorAll('.lock')
 var unlockBtnAll = document.querySelectorAll('.unlock')
-var unlockBtn1 = document.querySelector('.unlock1');
-var unlockBtn2 = document.querySelector('.unlock2');
-var unlockBtn3 = document.querySelector('.unlock3');
-var unlockBtn4 = document.querySelector('.unlock4');
-var unlockBtn5 = document.querySelector('.unlock5');
-var lockBtn1 = document.querySelector('.lock1');
-var lockBtn2 = document.querySelector('.lock2');
-var lockBtn3 = document.querySelector('.lock3');
-var lockBtn4 = document.querySelector('.lock4');
-var lockBtn5 = document.querySelector('.lock5');
 var itemInput = document.querySelector('.color-swatch')
 var savedPaletteImgs = document.querySelector('.color-array')
 var newPaletteBtn = document.querySelector('.new-pal-btn')
@@ -35,9 +25,9 @@ savedPaletteImgs.addEventListener('click', deletePalette)
 
 function deletePalette() {
   var itemToDelete = parseInt(event.target.id)
-  for (var i=0; i<savedPaletteArray.length; i++) {
+  for (var i = 0; i < savedPaletteArray.length; i++) {
     if (itemToDelete === savedPaletteArray[i].uniqueId) {
-      savedPaletteArray.splice(i,1)
+      savedPaletteArray.splice(i, 1)
       event.target.parentElement.remove()
       console.log(event.target.parentElement)
     }
@@ -58,10 +48,6 @@ newPaletteBtn.addEventListener('click', function() {
 })
 
 itemInput.addEventListener('click', toggleLock);
-itemInput.addEventListener('click', toggleLock2);
-itemInput.addEventListener('click', toggleLock3);
-itemInput.addEventListener('click', toggleLock4);
-itemInput.addEventListener('click', toggleLock5);
 
 function brandNewPalette() {
   currentPalette = new Palette
@@ -95,46 +81,14 @@ function show(element) {
 
 function toggleLock(event) {
   mouseClick = event.target.className
-  if (mouseClick === 'color color1') {
-    toggle(lockBtn1)
-    toggle(unlockBtn1)
-    currentPalette.colorPalette[0].locked = !currentPalette.colorPalette[0].locked
-  }
-}
-
-function toggleLock2(event) {
-  mouseClick = event.target.className
-  if (mouseClick === 'color color2') {
-    toggle(lockBtn2)
-    toggle(unlockBtn2)
-    currentPalette.colorPalette[1].locked = !currentPalette.colorPalette[1].locked
-  }
-}
-
-function toggleLock3(event) {
-  mouseClick = event.target.className
-  if (mouseClick === 'color color3') {
-    toggle(lockBtn3)
-    toggle(unlockBtn3)
-    currentPalette.colorPalette[2].locked = !currentPalette.colorPalette[2].locked
-  }
-}
-
-function toggleLock4(event) {
-  mouseClick = event.target.className
-  if (mouseClick === 'color color4') {
-    toggle(lockBtn4)
-    toggle(unlockBtn4)
-    currentPalette.colorPalette[3].locked = !currentPalette.colorPalette[3].locked
-  }
-}
-
-function toggleLock5(event) {
-  mouseClick = event.target.className
-  if (mouseClick === 'color color5') {
-    toggle(lockBtn5)
-    toggle(unlockBtn5)
-    currentPalette.colorPalette[4].locked = !currentPalette.colorPalette[4].locked
+  console.log(event.target.className)
+  for (var i = 0; i <= 4; i++) {
+    if (allColor[i].className === event.target.className) {
+      console.log(allColor[i])
+      toggle(lockBtnAll[i])
+      toggle(unlockBtnAll[i])
+      currentPalette.colorPalette[i].locked = !currentPalette.colorPalette[i].locked
+    }
   }
 }
 
@@ -143,13 +97,13 @@ function getRandomIndex(hexCodeArray) {
 }
 
 function changeHex(currentPalette) {
-  for (var i=0; i<=4; i++) {
+  for (var i = 0; i <= 4; i++) {
     allHex[i].innerText = currentPalette.colorPalette[i].ranHexCode
   }
 }
 
 function background(currentPalette) {
-  for (var i=0; i<=4; i++) {
+  for (var i = 0; i <= 4; i++) {
     allColor[i].style.backgroundColor = currentPalette.colorPalette[i].ranHexCode
   }
 }
